3f3b4ecb91adec9e208fcb8f804d470a5100fb60
Kyan820815 <patrickchang820815@gmail.com>
2022-05-27 18:51:19 +0800

    20220527-3 update code

--- lc09xx/lc0907.cpp
+++ lc09xx/lc0907.cpp
@@ -17,3 +17,23 @@ public:
         return res;
     }
 };
+
+//--- method 2: only increasing stack without dp
+class Solution {
+public:
+    using ll = long long;
+    int sumSubarrayMins(vector<int>& arr) {
+        ll n = arr.size(), mod = 1e9+7, res = 0;
+        vector<int> sk = {-1};
+        for (int i = 0; i <= n; ++i) {
+            int curval = i == n ? INT_MIN : arr[i];
+            while (sk.back() != -1 && arr[sk.back()] > curval) {
+                int idx = sk.back();
+                sk.pop_back();
+                res = (res + (idx-sk.back()) * (i-idx) * (ll)arr[idx]) % mod;
+            }
+            sk.push_back(i);
+        }
+        return res;
+    }
+};
--- lc21xx/lc2104.cpp
+++ lc21xx/lc2104.cpp
@@ -5,33 +5,53 @@ class Solution {
 public:
     using ll = long long;
     long long subArrayRanges(vector<int>& nums) {
-        vector<int> sk;
         int n = nums.size();
         ll res = 0;
+        vector<ll> minsk = {-1}, maxsk = {-1};
         for (int i = 0; i <= n; ++i) {
-            while (sk.size() && nums[sk.back()] > (i == n ? INT_MIN : nums[i])) {
-                int k = sk.back();
-                sk.pop_back();
-                int left = sk.size() ? sk.back() : -1;
-                res -= (ll)nums[k] * (k-left) * (i-k);
+            int curmin = i == n ? INT_MIN : nums[i];
+            int curmax = i == n ? INT_MAX : nums[i];
+            while (minsk.back() != -1 && nums[minsk.back()] > curmin) {
+                int idx = minsk.back();
+                minsk.pop_back();
+                res -= (idx-minsk.back())*(i-idx)*nums[idx];
             }
-            if (i < n) {
-                sk.push_back(i);
+            while (maxsk.back() != -1 && nums[maxsk.back()] < curmax) {
+                int idx = maxsk.back();
+                maxsk.pop_back();
+                res += (idx-maxsk.back())*(i-idx)*nums[idx];
             }
-        }        
-        sk.clear();
-        for (int i = 0; i <= n; ++i) {
-            while (sk.size() && nums[sk.back()] < (i == n ? INT_MAX : nums[i])) {
-                int k = sk.back();
-                sk.pop_back();
-                int left = sk.size() ? sk.back() : -1;
-                res += (ll)nums[k] * (k-left) * (i-k);
-            }
-            if (i < n) {
-                sk.push_back(i);
-            }
+            minsk.push_back(i);
+            maxsk.push_back(i);
         }
+        return res;
+    }
+};
 
+//--- method 2: dp with stack
+class Solution {
+public:
+    using ll = long long;
+    long long subArrayRanges(vector<int>& nums) {
+        int n = nums.size();
+        ll res = 0;
+        vector<ll> minsk = {-1}, maxsk = {-1};
+        vector<ll> mindp(n, 0), maxdp(n, 0);
+        for (int i = 0; i < n; ++i) {
+            while (minsk.back() != -1 && nums[minsk.back()] > nums[i]) {
+                minsk.pop_back();
+            }
+            while (maxsk.back() != -1 && nums[maxsk.back()] < nums[i]) {
+                maxsk.pop_back();
+            }
+            ll lastmin = minsk.back() != -1 ? mindp[minsk.back()] : 0;
+            ll lastmax = maxsk.back() != -1 ? maxdp[maxsk.back()] : 0;
+            mindp[i] = lastmin + (i-minsk.back())*nums[i];
+            maxdp[i] = lastmax + (i-maxsk.back())*nums[i];
+            minsk.push_back(i);
+            maxsk.push_back(i);
+            res += maxdp[i]-mindp[i];
+        }
         return res;
     }
 };
